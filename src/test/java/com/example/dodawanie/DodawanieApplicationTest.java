b62f52b59b5bf6b94d29e3878ccc9d1d27e10e15
Miłosz <milomal486@student.polsl.pl>
2023-04-19 13:36:30 +0200

    init v2

---
+++ src/test/java/com/example/dodawanie/DodawanieApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.dodawanie;
+
+import static org.junit.Assert.*;
+
+public class DodawanieApplicationTest {
+
+    @org.junit.Test
+    public void added() {
+        DodawanieApplication dodawanieApplication=new DodawanieApplication();
+        assertEquals(10,dodawanieApplication.added(5,5));
+ 
+    }
+}
